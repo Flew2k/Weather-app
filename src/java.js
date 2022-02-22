df2512286c096f7afedfc9c0d670808854b847b4
oyuka-y1 <oyuka-y1@iMAC2021-05s-iMac.local>
2022-02-22 15:18:40 +0800

    issuse11

--- src/java.js
+++ src/java.js
@@ -22,9 +22,31 @@ const api = {
     apiKey: "d20ecf927f11571e877e6c6227621742",
     baseUrl :'https://api.openweathermap.org/data/2.5/weather?q=',
 }
+let autocomplete;
+const input = document.querySelector(".searchBar");
 
-const input = document.querySelector('#search')
-input.addEventListener('keypress' , listener);
+function initAutocomplete(){
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById('autocomplete'),
+        {
+            types:['geocode'],
+            field:['name']
+        }
+    );
+    console.log(autocomplete);
+    autocomplete.addListener('place_change' , onPlaceChange)
+}
+
+function onPlaceChange(){
+    var place = autocomplete.getPlace();
+    if(!place.geometry){
+        document.getElementById('autocomplete').placeholder = 'Not you';
+    }else{
+        document.getElementById('autocomplete').innerHTML = place.name;
+    }
+}
+
+input.addEventListener("keypress" , listener);
 
 function listener(event){
     if(event.keyCode == 13){
